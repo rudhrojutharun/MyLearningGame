e23eea5a29b1c479ff0a5b3d0c8b7e93e25d4e3f
rudhrojutharun <rudhrojutharun@gmail.com>
2024-11-08 11:38:27 +0530

    updated changes in app.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import logo from './logo.svg';
 import './App.css';
 import AnimalSounds from './components/AnimalSounds/AnimalSounds';
 
+function Home() {
+  return <p>Welcome to the learning game! Choose an activity to start.</p>;
+}
 
 function App() {
   return (
     <Router>
 <div>
       <h1>Welcome to My Learning Game!</h1>
-      <p>Let's start learning with fun activities!</p>
       <nav>
           <Link to="/">Home</Link> | <Link to="/animal-sounds">Animal Sounds</Link>
       </nav>
 
 
 
-      <p> Click on Animal Sounds to know about Animal AnimalSounds</p>
       <Routes>
           {/* Home Route */}
           <Route path="/" element={<Home />} />
@@ -27,9 +28,7 @@ function App() {
         </Routes>
     </div>
     </Router>
-    function Home() {
-      return <p>Welcome to the learning game! Choose an activity to start.</p>;
-    }
+    
     
   );
 }
