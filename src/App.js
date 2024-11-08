cc2811ed7000c2cfa49dc7e97c61692612448c26
rudhrojutharun <rudhrojutharun@gmail.com>
2024-11-08 07:10:05 +0530

    Initial commit of my learning game

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Learning Game!</h1>
+      <p>Let's start learning with fun activities!</p>
+    </div>
+  );
+}
+
+export default App;
